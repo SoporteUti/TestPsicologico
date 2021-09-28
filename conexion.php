09f2dc8d4cc5ccefd4b8cdd161b973960e51eb80
SoporteUti <soporte.tifmp@ues.edu.sv>
2021-09-28 10:39:27 -0600

    conexion para que no de fallas

--- conexion.php
+++ conexion.php
@@ -2,9 +2,9 @@
 
 $hostname_conexion = "localhost";
 $username_conexion = "root";
-$password_conexion = "";
+$password_conexion = "UTIFMP";
 
-$conexion = mysqli_connect($hostname_conexion, $username_conexion, $password_conexion,"psicologica");
+$conexion = mysqli_connect($hostname_conexion, $username_conexion, $password_conexion,"psicologico");
 //mysql_select_db(eliseowe_testsicologico2020,$conexion);
 
 ?>
