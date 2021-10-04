a9d3ce8e52250c7d52988f2d53b2cc8db466da0b
SoporteUti <soporte.tifmp@ues.edu.sv>
2021-10-04 11:24:03 -0600

    cambios

--- conexion.php
+++ conexion.php
@@ -4,7 +4,7 @@ $hostname_conexion = "localhost";
 $username_conexion = "root";
 $password_conexion = "UTIFMP";
 
-$conexion = mysqli_connect($hostname_conexion, $username_conexion, $password_conexion,"psicologica");
+$conexion = mysqli_connect($hostname_conexion, $username_conexion, $password_conexion,"psicologico");
 //mysql_select_db(eliseowe_testsicologico2020,$conexion);
 
 ?>
